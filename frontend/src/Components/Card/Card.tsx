c4e6fd05c9704355b0a63adfe6025beb055b35b0
Thanosk97 <thanosk97@gmail.com>
2024-05-07 10:41:30 +0100

    Reacr frontend installation and card.tsx

---
+++ frontend/src/Components/Card/Card.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='card'>Card
+        <div className='details'>
+            <h2>APPL</h2>
+            <p className='info'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed, odit quibusdam. Amet, fugit tempora consequuntur aperiam doloremque possimus dolore aspernatur praesentium consequatur explicabo atque cupiditate, odit dicta maxime voluptas hic.</p>
+        </div>    
+    </div>
+  )
+}
+
+export default Card
